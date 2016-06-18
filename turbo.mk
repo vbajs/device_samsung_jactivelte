b63db57dadc6631a8da24e8a5689b7140a353ae1
Yahya Wessam <vbajs@users.noreply.github.com>
2016-06-18 15:01:46 -0700

    [DO NOT MERGE UNTIL STABLE] Make build fingerprint updated
    from 4.4.2 KTU84Q I9295XXUCNE5 to 5.0.1 LRX22C I9295XXUDOI5

--- turbo.mk
+++ turbo.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jactivelte \
     TARGET_DEVICE=jactivelte \
-    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:4.4.2/KTU84Q/I9295XXUCNE5:user/release-keys" \
-    PRIVATE_BUILD_DESC="jactivelte-user 4.4.2 KTU84Q I9295XXUCNE5 release-keys"
+    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:5.0.1/LRX22C/I9295XXUDOI5:user/release-keys" \
+    PRIVATE_BUILD_DESC="jactivelte-user 5.0.1 LRX22C I9295XXUDOI5 release-keys"
 
 PRODUCT_NAME := turbo_jactivelte
 PRODUCT_DEVICE := jactivelte
